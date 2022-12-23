174eb6ead7e8b0ce2a4b6e8ff8aec6bbd22be7d3
Gurwan <gurwandelaunay35@gmail.com>
2022-12-23 18:17:03 +0100

    notification works : to do -> change small icon + put steps for notification

--- android/app/src/main/java/com/okariastudio/addictedstalked/BackgroundService.java
+++ android/app/src/main/java/com/okariastudio/addictedstalked/BackgroundService.java
@@ -17,6 +17,7 @@ import android.os.PowerManager;
 import android.os.UserManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 import java.util.List;
 import java.util.Timer;
@@ -25,6 +26,12 @@ import java.util.TimerTask;
 public class BackgroundService extends Service {
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private final String CHANNEL_ID = "okariastudio.addictedstalker.alert";
+    private final int MINUTES_TEN = 600000;
+    private final int MINUTES_NINE = 540000;
+    private final int MINUTES_EIGHT = 480000;
+    private final int MINUTES_FIVE = 300000;
+    private final int MINUTES_FOUR = 240000;
 
     @Override
     public void onCreate() {
@@ -32,6 +39,11 @@ public class BackgroundService extends Service {
 
         String NOTIFICATION_CHANNEL_ID = "okariastudio.addictedstalker.persistant";
         NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Background Service", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationChannel channel2 = new NotificationChannel(CHANNEL_ID, "Alerte Addicted", NotificationManager.IMPORTANCE_DEFAULT);
+
+        NotificationManager manager2 = getSystemService(NotificationManager.class);
+        manager2.createNotificationChannel(channel2);
+
         channel.setLockscreenVisibility(-1);
         channel.setLightColor(Color.BLUE);
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
@@ -50,6 +62,18 @@ public class BackgroundService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         sharedPreferences = getSharedPreferences("Addicted Stalked",MODE_PRIVATE);
         editor = sharedPreferences.edit();
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Alerte : Addicted Stalked")
+                .setContentText("Oula ça fait déjà 10 minutes qu'est que tu fais de beau ?")
+                .setPriority(NotificationCompat.PRIORITY_MAX);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        editor.putInt("TEN_SENT",0);
+
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
@@ -65,7 +89,9 @@ public class BackgroundService extends Service {
                     for(UsageStats u : usageStatsList){
                         total += u.getTotalTimeInForeground();
                     }
+
                     editor.putInt("ALL_COUNTER_REAL",total);
+
                     if(!(manager.inKeyguardRestrictedInputMode())){
                         int totalBeforeLock = sharedPreferences.getInt("TIME_LAST_PAUSE",-1);
                         if(totalBeforeLock != -1){
@@ -74,6 +100,14 @@ public class BackgroundService extends Service {
                     } else {
                         editor.putInt("TIME_LAST_PAUSE",total);
                     }
+
+                    if(total > 600000 && (sharedPreferences.getInt("TEN_SENT",-1) == 0)){
+                        editor.putInt("TEN_SENT",1);
+                        notificationManager.notify(2, builder.build());
+                    } else if (total >= MINUTES_EIGHT && total <= MINUTES_NINE){
+                        editor.putInt("TEN_SENT",0);
+                    }
+
                     editor.putInt("ALL_COUNTER",total);
                     editor.apply();
                 }
